6e698d9f0118057f2042651fe13024043c8441b5
Issei Naruta <naruta@cookpad.com>
2013-07-17 22:59:43 +0900

    Improve daemons' options

--- bin/retro_control.rb
+++ bin/retro_control.rb
@@ -1,6 +1,14 @@
 #!/usr/bin/env ruby
 require 'bundler/setup'
 require 'daemons'
+require 'pathname'
 
-Daemons.run File.expand_path('../retro.rb', __FILE__)
-
+base_dir = Pathname('../../').expand_path(__FILE__)
+Daemons.run(
+  base_dir.join('bin/retro.rb'),
+  app_name: 'retrobot',
+  dir_mode: :normal,
+  dir: base_dir.join('tmp'),
+  log_dir: base_dir.join('log'),
+  log_output: true
+)
