860fcd96f27ad9b06c5e38cd53d33d1175f686b2
Issei Naruta <naruta@cookpad.com>
2014-06-18 21:24:17 +0900

    Add spec for retro_days filter

--- spec/lib/retrobot/tweet_filters_spec.rb
+++ spec/lib/retrobot/tweet_filters_spec.rb
@@ -1,12 +1,18 @@
+require 'retrobot/tweet'
 require 'retrobot/tweet_filters'
+require 'retrobot/config'
 require 'logger'
+require 'timecop'
 
 describe Retrobot::TweetFilters do
   let(:retrobot) {
-    double(:retrobot, logger: Logger.new('/dev/null'))
+    double(:retrobot, logger: Logger.new('/dev/null'), config: config)
   }
 
   let(:filter) { filter_class.new retrobot }
+  let(:config) do
+    Retrobot::Config.new
+  end
 
   describe 'AddInReplyToUrl#filter' do
     subject(:filter_class) { Retrobot::TweetFilters::AddInReplyToUrl }
@@ -19,6 +25,32 @@ describe Retrobot::TweetFilters do
       end
       tweet_after = filter.filter tweet_before
       expect(tweet_after.text).to eq('@mirakui_retro おめでとうございます https://twitter.com/mirakui_retro/status/419483520973565952')
+    end
+  end
+
+  describe 'RetroDays' do
+    subject(:filter_class) { Retrobot::TweetFilters::RetroDays }
+    let(:now) { Time.new(2014,01,01).localtime }
+    let(:config) do
+      Retrobot::Config.new retro_days: 365
+    end
+
+    it 'retrys if newer than retro_days' do
+      Timecop.freeze(now) do
+        tweet_before = Retrobot::Tweet.new.tap do |t|
+          t.timestamp = now - 364.days
+        end
+        expect{ filter.filter(tweet_before) }.to raise_error(Retrobot::TweetFilters::RetryLater)
+      end
+    end
+
+    it 'tweets tweet older than retro_days' do
+      Timecop.freeze(now) do
+        tweet_before = Retrobot::Tweet.new.tap do |t|
+          t.timestamp = now - 365.days
+        end
+        expect(filter.filter(tweet_before)).to eq(tweet_before)
+      end
     end
   end
 end
