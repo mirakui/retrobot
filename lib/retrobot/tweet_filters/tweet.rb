29a6f00197a8564625cb0dfa2d1fdcce97dc7835
Issei Naruta <naruta@cookpad.com>
2015-02-07 16:26:27 +0900

    Rewrite retryable syntax as 2.0.0 style https://github.com/nfedyashev/retryable/blob/master/CHANGELOG.md#retryable-200

--- lib/retrobot/tweet_filters/tweet.rb
+++ lib/retrobot/tweet_filters/tweet.rb
@@ -16,7 +16,7 @@ class Retrobot
       def tweet(text)
         logger.info "tweet: #{text}"
         return if config.dryrun
-        retryable(tries: config.retry_count, sleep: config.retry_interval) do
+        Retryable.retryable(tries: config.retry_count, sleep: config.retry_interval) do
           client.update text
         end
       end
